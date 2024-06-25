f40c56cc9f5003c3d13e6a4584f3413c329612cf
Gabriel Carneiro <biel.dev@outlook.com>
2024-06-25 20:06:48 -0300

    chore(versionamento): atualizacao versão.

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.22.0');
+define('PLUGIN_FIELDS_VERSION', '1.23.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "10.0.0");
